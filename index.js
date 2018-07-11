6d9700f7432caa9c031e661889c4732b00a4fba7
cdevries546 <cdevries@comcast.net>
2018-07-11 17:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your code in this file!
+var currentUser = "hello";
+var welcomeMessage = "Welcome to Klossybook, " + currentUser  + "!";
+const upperCaseCurrentUser = currentUser.toUpperCase();
+var excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, " + upperCaseCurrentUser + "!";
 
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
